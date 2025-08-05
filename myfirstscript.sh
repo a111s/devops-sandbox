0eed7d11dad59a6d8ef0ddb14f986fe9f36bf600
Akshara Sinha <aksharasinha@Mac.attlocal.net>
2025-08-05 03:31:57 -0700

    -ymissingearlier

--- myfirstscript.sh
+++ myfirstscript.sh
@@ -1,6 +1,6 @@
 # This is a test
 sudo apt install apache2 -y
 sudo apt install mysql-server-8.0 -y
-sudo apt install postgresql-server-dev-all
-sudo apt install gnupg
-sudo apt install curl
+sudo apt install postgresql-server-dev-all -y
+sudo apt install gnupg -y
+sudo apt install curl -y
